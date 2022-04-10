754a80cc8d8c1837b287b67298372008f3153081
kinnlm <34890675+kinnlm@users.noreply.github.com>
2022-04-10 05:54:59 -0400

    Add files via upload

---
+++ src/QuickSort003.java
@@ -0,0 +1,47 @@
+/*
+ * Class: QuickSort003.JAVA
+ * @Author Logan Kinnaird
+ * @Version
+ * Course ITEC 2150-07 Spring 2022
+ * Written: Apr 10, 2022
+ *
+ * This class - Quick sort for strings
+ */
+
+public class QuickSort003
+{
+	public int counter = 0;
+
+	int partition(String[] list, int beginning, int end)
+	{
+		String pivot = list[end];
+		int i = beginning - 1;
+
+		for (int j = beginning; j <= end; j++)
+		{
+			if (list[i].compareTo(pivot) < 0)
+			{
+				i++;
+				String storageVariable = list[i];
+				list[i] = list[j];
+				list[j] = storageVariable;
+			}
+		}
+
+		String storageVariable = list[i + 1];
+		list[i + 1] = list[end];
+		list[end] = storageVariable;
+		return (i + 1);
+	}
+
+	void quickSort002(String[] list, int beginning, int end)
+	{
+		++counter;
+		if (beginning < end)
+		{
+			int partIndex = partition(list, beginning, end);
+			quickSort002(list, beginning, partIndex - 1);
+			quickSort002(list, partIndex + 1, end);
+		}
+	}
+}

ee3c529a84038cdd73dd7e1f5deb031bc4174475
kinnlm <d39aaeee@opayq.com>
2022-04-15 01:37:50 -0400

    Initial commit

--- src/QuickSortInt.java
+++ src/QuickSortInt.java
@@ -1,18 +1,22 @@
 /*
- * Class: QuickSort002.JAVA
+ * Class: QuickSortInt.JAVA
  * @Author Logan Kinnaird
- * @Version 1.0
+ * @Version 1.3
  * Course ITEC 2150-07 Spring 2022
  * Written: Apr 10, 2022
  *
- * This class - Algorithim to order data something of a hybrid between a bubble sort and a binary sort
+ * This class - Algorithm to order data something of a hybrid between a bubble sort and a binary sort
  */
 
 // @see https://www.javatpoint.com/quick-sort
 
-public class QuickSort002
+public class QuickSortInt
 {
+/**
+ * @param counter count how many steps it takes to order data
+ */
 	public int counter = 0;
+
 	int partition(int[] list, int beginning, int end)
 	{
 		int pivot = list[end];
@@ -23,9 +27,9 @@ public class QuickSort002
 			if (list[i] < pivot)
 			{
 				i++;
-				int storageVariable = list[i];
+				int tempStorageVariable = list[i];
 				list[i] = list[j];
-				list[j] = storageVariable;
+				list[j] = tempStorageVariable;
 			}
 		}
 
